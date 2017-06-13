11a744c17845a465240880efce4cebd4b94b6bc5
Ken Russo <krrusso@gmail.com>
2017-06-13 10:34:34 -0400

    update1

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-// const express = require('express')
-// const app = express();
-// const server = http.require;
-
-// // app.get('/', function (req, res){
-// // 	server.send("test");
-// // })
-
-// // app.listen(8080, () => {
-// // 	res.end("server listening");
-// // })
-
-
 const express = require('express')
 const app = express()
-// const http = require("http")
+const nun = require('nunjucks')
+app.set('view engine', 'html'); 
+app.engine('html', nun.render); 
+nun.configure('views', { noCache: true }); 
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+
+
+
+nun.render('index', locals, function (err, output) {
+	console.log (output);
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/', function (req, res) {
+res.render ( 'views/index', {title: 'Hall of Fame', people: people })
+})
+
 app.listen(3030, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3030!')
   
 })
 
+
+var locals = {
+	title: 'An Example',
+	people: [
+	{ name: 'Gandalf'},
+	{ name: 'Frodo' },
+	{ name: 'Hermione'}
+	]
+}
 
